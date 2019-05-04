a58eb799a4be492cd89b6bef47551686cec1baa8
fsubal <fsubal1102@gmail.com>
2019-05-05 03:43:05 +0900

    give structure

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import Canvas from "./canvas";
+import Canvas from "./src/canvas";
 
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.querySelector<HTMLCanvasElement>('#app')
